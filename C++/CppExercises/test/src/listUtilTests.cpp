81ffa4815492ab695bca5aae1688f550844d1e92
mapope2 <mapope2@illinois.edu>
2021-02-24 22:41:12 -0600

    Add clang-format file for the project

--- C++/CppExercises/test/src/listUtilTests.cpp
+++ C++/CppExercises/test/src/listUtilTests.cpp
@@ -8,27 +8,48 @@
 
 #include "ListNode.hpp"
 #include "ListNodeUtilities.hpp"
-#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 TEST(sortList, AscendingOrder) {
-  ListNode l1(1);
-  ListNode l2(2);
-  ListNode l3(3);
-  ListNode l4(4);
+    ListNode l1(1);
+    ListNode l2(2);
+    ListNode l3(3);
+    ListNode l4(4);
 
-  l4.next = &l2;
-  l2.next = &l1;
-  l1.next = &l3;
+    l4.next = &l2;
+    l2.next = &l1;
+    l1.next = &l3;
 
-  ListNode *result = ListNodeUtilities::insertionSort(&l4);
-  EXPECT_EQ(result->val, 1);
-  result = result->next;
-  EXPECT_EQ(result->val, 2);
-  result = result->next;
-  EXPECT_EQ(result->val, 3);
-  result = result->next;
-  EXPECT_EQ(result->val, 4);
-  result = result->next;
-  EXPECT_EQ(result, nullptr);
+    ListNode *result = ListNodeUtilities::insertionSort(&l4);
+    EXPECT_EQ(result->val, 1);
+    result = result->next;
+    EXPECT_EQ(result->val, 2);
+    result = result->next;
+    EXPECT_EQ(result->val, 3);
+    result = result->next;
+    EXPECT_EQ(result->val, 4);
+    result = result->next;
+    EXPECT_EQ(result, nullptr);
+}
+
+TEST(Merge, TwoNonNegativeLists) {
+    ListNode<int> l3(3);
+    ListNode<int> l2(4, &l3);
+    ListNode<int> l1(2, &l2);
+
+    ListNode<int> l6(4);
+    ListNode<int> l5(6, &l6);
+    ListNode<int> l4(5, &l5);
+
+    //  ListNode *result = ListNodeUtilities::insertionSort(&l4);
+    auto result = ListNodeUtilities::addTwoNumbers(&l1, &l4);
+    //  EXPECT_EQ(result->val, 1);
+    //  result = result->next;
+    //  EXPECT_EQ(result->val, 2);
+    //  result = result->next;
+    //  EXPECT_EQ(result->val, 3);
+    //  result = result->next;
+    //  EXPECT_EQ(result->val, 4);
+    //  result = result->next;
+    //  EXPECT_EQ(result, nullptr);
 }
