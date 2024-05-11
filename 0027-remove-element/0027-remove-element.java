d8eb57d003c4c83bd524c8836710d8774d4bc3f8
Michael Pope <36758185+MichaelAnthonyP96@users.noreply.github.com>
2024-05-10 19:22:48 -0700

    Time: 0 ms (100%), Space: 41.4 MB (92.55%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int swapPos = findLastNonVal(nums, val, nums.length-1);
+        for (int i = 0; i < swapPos; ++i) {
+            if (nums[i] == val) {
+                // swap the target number with a nonVal at the end of the list
+                swap(nums, swapPos, i);
+                // find the next swapPosition
+                swapPos = findLastNonVal(nums, val, swapPos);
+            }
+        }
+        return (swapPos + 1);
+    }
+    
+    public void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+    
+    public int findLastNonVal(int nums[], int val, int start) {
+        for (int j = start; j >= 0; --j) {
+            if (nums[j] != val) {
+                return j;
+            }       
+        }
+        // we didn't find a value to swap, meaning we are done and this marks
+        // the count of how many we swapped
+        return -1;
+    }
+}
